8994c1b632d655cc5f7be7fc85e5111adda45a4f
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-04-26 19:47:09 +0900

    Add hotels information

--- src/components/HotelsTable.jsx
+++ src/components/HotelsTable.jsx
@@ -7,8 +7,14 @@ const HotelsTable = ({ hotels }) => (
   <table>
     <tbody>
       <tr>
+        <th>画像</th>
         <th>ホテル名</th>
+        <th className="hotel-price-column">値段</th>
+        <th>レビュー</th>
+        <th>レビュー件数</th>
+        <th>距離</th>
       </tr>
+      {/* ホテルの検索結果が以下に列挙される */}
       {hotels.map((hotel) => (
         <HotelRow key={hotel.id} hotel={hotel} />
       ))}
--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,3 +1,5 @@
+import getDistance from 'geolib/es/getDistance';
+
 import Rakuten from '../lib/Rakuten';
 
 export const searchHotelByLocation = (location) => {
@@ -11,10 +13,21 @@ export const searchHotelByLocation = (location) => {
   return Rakuten.Travel.simpleHotelSearch(params).then((result) =>
     result.data.hotels.map((hotel) => {
       const basicInfo = hotel.hotel[0].hotelBasicInfo;
+      const price = basicInfo.hotelMinCharge;
+      const distance = getDistance(
+        { latitude: location.lat, longitude: location.lng },
+        { latitude: basicInfo.latitude, longitude: basicInfo.longitude }
+      );
+
       return {
         id: basicInfo.hotelNo,
         name: basicInfo.hotelName,
         url: basicInfo.hotelInformationUrl,
+        thumbUrl: basicInfo.hotelThumbnailUrl,
+        price: price ? `${price}円` : '空室なし',
+        reviewAverage: basicInfo.reviewAverage,
+        reviewCount: basicInfo.reviewCount,
+        distance,
       };
     })
   );

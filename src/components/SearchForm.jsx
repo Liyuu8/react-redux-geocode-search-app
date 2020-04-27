ce54a8023b2fde3eb9e07e1ab1496bd9ba626b88
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-04-28 02:48:11 +0900

    Implement the React Router

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,43 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class SearchForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      place: '東京タワー',
-    };
-  }
-
-  handlePlaceChanged(place) {
-    this.setState({ place });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onSubmit(this.state.place);
-  }
-
-  render() {
-    return (
-      <form
-        className="search-form"
-        onSubmit={(event) => this.handleSubmit(event)}
-      >
-        <input
-          className="place-input"
-          type="text"
-          size="30"
-          value={this.state.place}
-          onChange={(event) => this.handlePlaceChanged(event.target.value)}
-        />
-        <input className="submit-button" type="submit" value="検索" />
-      </form>
-    );
-  }
-}
+const SearchForm = (props) => (
+  <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
+    <input
+      className="place-input"
+      type="text"
+      size="30"
+      value={props.place}
+      onChange={(e) => props.onPlaceChange(e.target.value)}
+    />
+    <input className="submit-button" type="submit" value="検索" />
+  </form>
+);
 
 SearchForm.propTypes = {
+  place: PropTypes.string.isRequired,
+  onPlaceChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
 
